89035f7be796b9f70368081803bc65ad8cb041a1
TheEmon <mdemrannaziremon@gmail.com>
2022-03-14 06:39:38 +0600

    IF STATEMENT CODE

---
+++ JS Control Flow/js/script.js
@@ -0,0 +1,16 @@
+//IF STATEMENT
+
+let age = 16;
+let state = 'CA';
+
+if (state == 'CA') {
+    if (age >= 16) {
+        console.log('You can drive');
+    }
+}
+
+// OR
+
+if (state == 'CA' && age == 16) {
+    console.log('You can drive');
+}
